0c94d63d33da471b12c6b6b35c7b20a98fe270c3
SHARVARI68 <75483333+SHARVARI68@users.noreply.github.com>
2021-06-19 20:16:25 +0530

    Update create.sql

--- create.sql
+++ create.sql
@@ -1,23 +1,22 @@
-CREATE TABLE publisher (
-    id INTEGER PRIMARY KEY,
-    name TEXT ,
-    country TEXT
-  );
-  
-CREATE TABLE books(
-  id INTEGER PRIMARY KEY,
-  title TEXT,
-  publisher INT REFERENCES publisher(id)
-  );
-  
-CREATE TABLE subjects(
-  id INTEGER PRIMARY KEY,
-  name TEXT
-  );
-  
-CREATE TABLE books_subjects(
-  book INT REFERENCES books(id),
-  subject INT REFERENCES subjects(id)
-  );
-  
-  
+PRAGMA foreign_keys = ON;
+create table publisher(
+       id integer primary key,
+       name text not NULL,
+       country text not NULL
+);
+create table books(
+       id  integer primary key,
+       title text not NULL,
+       publisher int,
+       FOREIGN KEY(publisher) REFERENCES publisher(id)
+);
+create table subjects(
+       id integer primary key,
+       name text not NULL
+);
+create table books_subjects(
+       book int,
+       subject int,
+       FOREIGN KEY(book) REFERENCES books(id),
+       FOREIGN KEY(subject) REFERENCES subjects(id)
+);
